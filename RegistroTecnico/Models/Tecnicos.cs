d9e3f83cbff8a5fff548d1fc1c18bc54737c40a2
elpollo-05 <joseamadobritorosario5@gmail.com>
2024-09-07 12:11:45 -0400

    Listo

--- RegistroTecnico/Models/Tecnicos.cs
+++ RegistroTecnico/Models/Tecnicos.cs
@@ -1,13 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace RegistroTecnico.Models
-{
+namespace RegistroTecnico.Models;
 
     public class Tecnicos
     {
         [Key]
         public int TecniCold { get; set; }
+
+        [Required]
         public string? Nombre { get; set; }
+        [Required]
         public double SueldoHora { get; set; }
-    }
 }
